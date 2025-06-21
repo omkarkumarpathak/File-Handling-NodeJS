a539a838bc0a23d561a498b9f3737cfa82b1f958
omkarkumarpathak <134271180+omkarkumarpathak@users.noreply.github.com>
2025-06-21 17:28:19 +0530

    Program to append at the last inside a file

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.get('/file/write',(req,res)=>{
 });
 
 
+//append text at the end of file
+
+app.get('/file/append',(req,res)=>{
+    fs.appendFile('./omkar.txt', '\n Greatest King in the World',(err)=>{
+        if(err){
+            return res.status(500).json({message:err});
+        }
+        
+        res.sendFile(__dirname + '/omkar.txt');
+    })
+})
+
+
+
 app.listen(4000,()=>{
     console.log("Server is running");
 })
